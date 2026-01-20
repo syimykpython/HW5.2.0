7d63fe4faeaf0bf04a39b9ee7cd0f465904075e2
sema_04 <syimykiliasov097@gmail.com>
2026-01-20 12:43:59 +0600

    hw6

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -1,8 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import (
+    CategoryListAPIView,
+    CategoryDetailAPIView,
+    ProductListAPIView,
+    ProductDetailAPIView,
+    ReviewListAPIView,
+    ReviewDetailAPIView,
+    ProductReviewsAPIView,
+)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/", include("product.urls")),
-    path("api/v1/users/", include("users.urls")),
+    path("categories/", CategoryListAPIView.as_view()),
+    path("categories/<int:id>/", CategoryDetailAPIView.as_view()),
+
+    path("products/", ProductListAPIView.as_view()),
+    path("products/<int:id>/", ProductDetailAPIView.as_view()),
+    path("products/reviews/", ProductReviewsAPIView.as_view()),
+
+    path("reviews/", ReviewListAPIView.as_view()),
+    path("reviews/<int:id>/", ReviewDetailAPIView.as_view()),
 ]
